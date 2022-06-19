30b8e6620e645468ebdcd3c1f1939dd1a75502de
vaninivan <44672972+vaninivan@users.noreply.github.com>
2022-06-19 22:19:46 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from wsgiref.simple_server import make_server
 
-GREETING = b'Hello, world! TEST   \n'
+GREETING = b'Hello, world! TESTING 123  \n'
 
 def hello(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
